7e6a591841964a49500fcbd2b22b9c9aeddd8e83
Sumit90557 <sumitrajpoot38951@gmail.com>
2025-07-01 13:30:18 +0530

    fix: updated homepage route

--- app.js
+++ app.js
@@ -88,6 +88,7 @@ app.use((req , res , next)=>{
 app.get('/', (req, res) => {
   res.render('listings/index');
 });
+
 // app.get("/demouser", async (req, res) => {
 //     try {
 //         let fakeUser = new User({
